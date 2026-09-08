b8d283d5f01323a3525336e624e740915ff4c520
Bhairy Srimanaswini <manaswinibhairy@gmail.com>
2026-09-08 20:54:01 +0530

    Time: 0 ms (100%), Space: 9.8 MB (29.02%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> arr(numRows);  // resize outer vector
+
+        for(int i = 0; i < numRows; i++) {
+            arr[i].resize(i+1);           // resize each row
+            arr[i][0] = 1;                // first element = 1
+            arr[i][i] = 1;                // last element = 1
+
+            for(int j = 1; j < i; j++) {
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j]; // Pascal’s rule
+            }
+        }
+
+        return arr;
+    }
+};
